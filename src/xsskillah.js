f469c8e1ea155ae2257927ba2a643bacffc25576
Julien Etienne <j8@envidia.io>
2024-03-31 19:32:00 +0100

    Added excludeClass option

--- src/xsskillah.js
+++ src/xsskillah.js
@@ -1,16 +1,19 @@
 
+const empty = ''
+
 const sandboxes = new Set()
 
 
 const globalDefaults = {
   allowTags: [],      // Allow dangerous tags
   allowTagRules: [],  // Allow dangerous tag rules
-  xssDocReset: 8
+  xssDocReset: 8,      // Number of sandboxes to create until resetting
   //        - inputTypeJS           ->      Allow inputMarkup type to be text/javascript
   //        - formAction            ->      Allow form ation attribute
   //        - strayDoubleQuotes     ->      Allow stray double quotes
   //        - straySingleQuotes     ->      Allow stray single quotes
   //        - strayBackTicks        ->      Allow stray back ticks
+  excludeClass: []
 }
 
 
@@ -77,13 +80,14 @@ const xssKillah = (globalOptions = {}) => (inputMarkup, instanceOptions) => {
   // Instance Options > Global Options > Global Defaults.
   const allowTags = instanceOptions?.allowTags || globalOptions?.allowTags || globalDefaults.allowTags
   const allowTagRules = instanceOptions?.allowTagRules || globalOptions?.allowTagRules || globalDefaults.allowTagRules
-
+  const excludeClass = instanceOptions?.excludeClass || globalOptions?.excludeClass || globalDefaults.excludeClass
+  console.log('excludeClass', excludeClass)
   // Set allowed tag rules object.
   for (const value of allowTagRules) allowTagRulesObj[value] = true
 
   // Vulnerable tags are ignored by default unless overriden by the global options or an instance option.
   const vulnerableTagsFiltered = vulnerableTags.filter(tag => !(allowTags).includes(tag))
-  
+
   // Remove filtered vulnerable tags
   vulnerableTagsFiltered.forEach(tag => {
     const tags = sandbox.querySelectorAll(tag)
@@ -107,6 +111,14 @@ const xssKillah = (globalOptions = {}) => (inputMarkup, instanceOptions) => {
         for (const attr of node.attributes) {
           const value = attr.value
           const key = attr.name
+
+          if (key === 'class') {
+            const regexPattern = '\\b(?:' + excludeClass.join('|') + ')\\b'
+            const exclusionRegex = new RegExp(regexPattern, 'g')
+            const newClassValue = value.replace(exclusionRegex, empty).trim().replace(/\s+/g, ' ')
+            node.removeAttribute('class')
+            node.setAttribute('class', newClassValue)
+          }
 
           // Vulnerable input
           if (name === 'input' && key === 'type' && value === 'text/javascript' && allowTagRulesObj.inputTypeJS) {
